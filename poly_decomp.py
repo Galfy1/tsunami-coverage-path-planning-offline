1a6e6755186db2b6c07ff9b26a8ef937cf2da7e2
Niels Sparre <fynniels12@gmail.com>
2025-10-29 20:03:02 +0100

    sidequest progress

--- poly_decomp.py
+++ poly_decomp.py
@@ -3,7 +3,7 @@
 # Decomp method based on https://arxiv.org/abs/2505.08060v1
 
 import csv
-from shapely.geometry import Polygon, Point
+from shapely.geometry import Polygon, Point, LineString
 import numpy as np
 import math
 
@@ -38,8 +38,64 @@ def main(args=None) -> None:
 
     fly_grid, home_cell, x_axis_coords, y_axis_coords, grid_res_x, grid_res_y = create_grid_from_polygon_and_noflyzones(
                                                                                         polygon, [], DRONE_START, CAMERA_COVERAGE_LEN)
+    # (fly_grid is a 2D numpy array where 1 = flyable, 0 = no-fly zone - (y,x) indexing)
 
 
+
+    # go though each "candidate sweep line" and check for non-monotone sections
+
+    non_monotone_sweep_lines = []
+
+    # go through all horizontal sweep lines:
+    p1_x = 0
+    p2_x = len(x_axis_coords)
+
+    non_monotone_in_x = False
+    non_monotone_in_x = False
+
+    for y in range(fly_grid.shape[0]):
+        intersection_points = []
+        # check intersection with fly_grid by going through the sweep line and check for 1-->0 or 0-->1 transitions
+        val = 0
+        for x in range(fly_grid.shape[1]):
+            cell_val = fly_grid[y][x]
+            if cell_val != val:
+                # transition detected
+                intersection_points.append( (y,x) )
+                val = cell_val
+            
+        if len(intersection_points) > 2:
+            # Sweep line is does not pass criteria! (its intersecting non-monotone sections)
+            sweep_line = LineString([ (p1_x, y_axis_coords[y]), (p2_x, y_axis_coords[y]) ])
+            gap_severity = 0
+            # for i in range(0, len(intersection_points)-1, 2):
+            #     start_pt = intersection_points[i]
+            #     end_pt = intersection_points[i+1]
+            #     gap_severity += abs(end_pt[0] - start_pt[0]) + abs(end_pt[1] - start_pt[1])
+                
+            non_monotone_sweep_lines.append( (sweep_line, intersection_points) )
+            non_monotone_in_y = True
+
+    # go through all vertical sweep lines:
+
+    # Check if "polygon" is irregular (i.e., non–monotone in both directions)
+    if non_monotone_in_x and non_monotone_in_y:
+        print("Polygon is irregular (non-monotone in both directions)")
+        # FIND SWEEP LINE MED STØRSTE GAP - OG SPLIT POLYGON HER.. OG GØR ALT IGEN INDTIL (non_monotone_in_x and non_monotone_in_y) ER FALSE
+    else:
+        print("Polygon is monotone in at least one direction")
+        # STOP HER!!! VI ER DONE FOR NU
+
+
+    # # Now process the non-monotone sweep lines to find the actual non-monotone sections
+    # non_monotone_sections = []
+    # for sweep_line, points in non_monotone_sweep_lines:
+    #     points.sort(key=lambda p: p[0])  # Sort by x coordinate
+    #     for i in range(len(points)-1):
+    #         current_end = points[i+1]
+    #         next_start = points[i+2] if i+2 < len(points) else None
+    #         if next_start is not None and next_start < current_end:
+    #             non_monotone_sections.append((sweep_line, current_end, next_start)) # (sweep line, current end, next start)
 
 if __name__ == '__main__':
     main()
